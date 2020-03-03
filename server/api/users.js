73e0ed8e725612a4ea62b74e9b513b9baa67d88e
luky1300 <jstrelnikova@gmail.com>
2020-03-03 13:09:14 -0500

    route for removing  item from  users cart was added

--- server/api/users.js
+++ server/api/users.js
@@ -48,7 +48,7 @@ router.get('/:userId', isAdmin, async (req, res, next) => {
 })
 
 //Route to add item to the cart
-//TBD - Need to protect the Route, should be available for users only
+//TBD - Need to protect the Route, should be available for the user only
 //TBD - Need to handle errors
 //TBD - We should not be able to add more items to the cart, than total quantity for the product
 router.post('/:userId/cart/:productId', async (req, res, next) => {
@@ -64,6 +64,29 @@ router.post('/:userId/cart/:productId', async (req, res, next) => {
     } else {
       const user = await User.findByPk(req.params.userId)
       await user.addProduct(req.params.productId)
+    }
+    res.json(item)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Route to remove item from the cart
+//TBD - Need to protect the Route, should be available for the user only
+//TBD - Need to handle errors
+router.delete('/:userId/cart/:productId', async (req, res, next) => {
+  try {
+    const item = await Cart.findOne({
+      where: {
+        productId: req.params.productId,
+        userId: req.params.userId
+      },
+    })
+    if (item.quantity > 1) {
+      await item.decrement('quantity', { by: 1 })
+    } else {
+      const user = await User.findByPk(req.params.userId)
+      await user.removeProduct(req.params.productId)
     }
     res.json(item)
   } catch (error) {
