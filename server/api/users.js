6ad437fefbdf14ad5497cc80a436817a28c41287
luky1300 <jstrelnikova@gmail.com>
2020-03-03 13:02:27 -0500

    defaultValue for quantity was changed for Cart model; route for adding item to users cart was added

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Cart} = require('../db/models')
 module.exports = router
 
 
@@ -14,9 +14,9 @@ const isAdmin = (req, res, next) => {
   }
 }
 
-// User Authorization 
-const isUser = (req, res, next) => { 
-  if(!req.user.id) { 
+// User Authorization
+const isUser = (req, res, next) => {
+  if(!req.user.id) {
     const err = new Error('Unauthorized Permission')
     res.status(401).send(err)
     next(err)
@@ -42,6 +42,30 @@ router.get('/:userId', isAdmin, async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId)
     res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Route to add item to the cart
+//TBD - Need to protect the Route, should be available for users only
+//TBD - Need to handle errors
+//TBD - We should not be able to add more items to the cart, than total quantity for the product
+router.post('/:userId/cart/:productId', async (req, res, next) => {
+  try {
+    const item = await Cart.findOne({
+      where: {
+        productId: req.params.productId,
+        userId: req.params.userId
+      },
+    })
+    if (item) {
+      await item.increment('quantity', { by: 1 })
+    } else {
+      const user = await User.findByPk(req.params.userId)
+      await user.addProduct(req.params.productId)
+    }
+    res.json(item)
   } catch (error) {
     next(error)
   }
