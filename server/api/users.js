ee23d1356b8da8beb725cee6b927b5a495c5bf69
luky1300 <jstrelnikova@gmail.com>
2020-03-04 15:32:13 -0500

    working put route to add/increment/decrement quantity in the cart

--- server/api/users.js
+++ server/api/users.js
@@ -66,6 +66,8 @@ router.get('/:userId', async (req, res, next) => {
 });
 
 //Route to add item to the cart
+
+//Route to change quantity of existing items in the cart
 //TBD - Need to protect the Route, should be available for the user only
 //TBD - Need to handle errors
 //TBD - We should not be able to add more items to the cart, than total quantity for the product
@@ -73,6 +75,8 @@ router.put('/:userId/cart', async (req, res, next) => {
   try {
     const orderId = req.body.orderId;
     const productId = req.body.productId;
+    const action = req.body.action;
+    let order;
     let item = await Cart.findOne({
       where: {
         productId: productId,
@@ -80,10 +84,15 @@ router.put('/:userId/cart', async (req, res, next) => {
       }
     });
     if (item) {
-      await item.increment('quantity', {by: 1});
+      if (action === 'add') {
+        await item.increment('quantity', {by: 1});
+      } else if (action === 'subtract' && item.quantity > 1) {
+        await item.decrement('quantity', {by: 1});
+      }
     } else {
-      const order = await Order.findByPk(orderId);
+      order = await Order.findByPk(orderId);
       item = await order.addProduct(productId);
+      item = item[0];
     }
     res.json(item);
   } catch (error) {
@@ -94,12 +103,14 @@ router.put('/:userId/cart', async (req, res, next) => {
 //Route to remove item from the cart
 //TBD - Need to protect the Route, should be available for the user only
 //TBD - Need to handle errors
-router.delete('/:userId/cart/:productId', async (req, res, next) => {
+router.delete('/:userId/cart', async (req, res, next) => {
+  const orderId = req.body.orderId;
+  const productId = req.body.productId;
   try {
-    const item = await Cart.findOne({
+    const item = await Order.findOne({
       where: {
-        productId: req.params.productId,
-        userId: req.params.userId
+        productId: productId,
+        orderId: orderId
       }
     });
     if (item.quantity > 1) {
