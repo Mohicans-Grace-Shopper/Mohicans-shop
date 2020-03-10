9b3753d5861923f8112a5ce209ad306762e25419
Isabel Jao <isabeljao@gmail.com>
2020-03-10 16:27:46 -0400

    fixed remove button in cart

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -4,8 +4,6 @@ const db = require('../db');
 const Cart = db.define('cart', {
   quantity: {
     type: Sequelize.INTEGER,
-    //defaultValue: 1, so when product associated with user for the first time, its initial quantity in the cart should be 1
-
     defaultValue: 0
   },
   itemPrice: {
